f0b7eb5bc4f5be5867eb29aedf08154d7bb51c3c
akkoucai <77622018+akkoucai@users.noreply.github.com>
2021-03-23 20:29:53 +0100

    Add files via upload

--- commands/Alien/checkbalance.js
+++ commands/Alien/checkbalance.js
@@ -1,40 +1,42 @@
 const { MESSAGES } = require("../../util/constants");
 const {MessageEmbed} = require('discord.js');
 module.exports.run = async (client,message,args) => {
-    
-    if(!args.length) {
-        let user = await client.getUser(message.member.user);
-        const embed = new MessageEmbed()
-        .setAuthor(`${message.member.displayName}`,message.member.user.displayAvatarURL())
-        .setTimestamp();
-        if(!user.accounts.length) {
-            embed
-            .addField(`:warning:`,`Vous n'avez pas encore de compte, plus d'informations avec \`!help addaccount\``)
-            .setColor("#dc5500")
-        }else {
-            for(const accName of user.accounts){
-                await client.updateBalance(accName,message.guild);
-                let acc = await client.getAccount(accName);
-                embed
-                .addField(`${acc.name}`,`Nombre de WAX : ${acc.nbWAX}\nNombre de TLM : ${acc.nbTLM}`)
-                .setColor("#006699");
-            }
-        }
-        
-        message.channel.send(embed);
-    }else {
-        await client.updateBalance(args[0],message.guild);
-        let acc = await client.getAccount(args[0])
-        const embed = new MessageEmbed()
-            .setAuthor(`${message.member.displayName} (${acc.name})`,message.member.user.displayAvatarURL())
-            .addFields(
-                {name:'Nombre de WAX: ', value : acc.nbWAX, inline: true},
-                {name:'Nombre de TLM: ', value : acc.nbTLM, inline: true}
-            )
-            .setColor("#006699")
+    setTimeout(async () => {
+        if(!args.length) {
+            let user = await client.getUser(message.member.user);
+            const embed = new MessageEmbed()
+            .setAuthor(`${message.member.displayName}`,message.member.user.displayAvatarURL())
             .setTimestamp();
+            if(!user.accounts.length) {
+                embed
+                .addField(`:warning:`,`Vous n'avez pas encore de compte, plus d'informations avec \`!help addaccount\``)
+                .setColor("#dc5500")
+            }else {
+                for(const accName of user.accounts){
+                    await client.updateBalance(accName,message.guild);
+                    let acc = await client.getAccount(accName);
+                    embed
+                    .addField(`${acc.name}`,`Nombre de WAX : ${acc.nbWAX}\nNombre de TLM : ${acc.nbTLM}`)
+                    .setColor("#006699");
+                }
+            }
+            
             message.channel.send(embed);
-    }
+        }else {
+            await client.updateBalance(args[0],message.guild);
+            let acc = await client.getAccount(args[0])
+            const embed = new MessageEmbed()
+                .setAuthor(`${message.member.displayName} (${acc.name})`,message.member.user.displayAvatarURL())
+                .addFields(
+                    {name:'Nombre de WAX: ', value : acc.nbWAX, inline: true},
+                    {name:'Nombre de TLM: ', value : acc.nbTLM, inline: true}
+                )
+                .setColor("#006699")
+                .setTimestamp();
+                message.channel.send(embed);
+        }
+    },1000);
+    
 
 };
 
