5547c2a4fbc417c6e8e6acace02569246c6dfcab
akkoucai <aimene.akkouche@grenoble-inp.org>
2021-03-24 19:53:29 +0100

    ajout wax et tlm total

--- commands/Alien/checkbalance.js
+++ commands/Alien/checkbalance.js
@@ -3,6 +3,8 @@ const {MessageEmbed} = require('discord.js');
 module.exports.run = async (client,message,args) => {
 
         if(!args.length) {
+            let nbWax = 0;
+            let nbTlm = 0;
             let user = await client.getUser(message.member.user);
             let embed = new MessageEmbed()
             .setAuthor(`${message.member.displayName}`,message.member.user.displayAvatarURL())
@@ -15,12 +17,14 @@ module.exports.run = async (client,message,args) => {
                 for(const accName of user.accounts){
                     setTimeout(() => client.updateBalance(accName, message.guild), 500)
                     let acc = await client.getAccount(accName);
+                    nbWax += acc.nbWAX;
+                    nbTlm += acc.nbTLM;
                     embed
                         .addField(`${acc.name}`,`Nombre de WAX : ${acc.nbWAX}\nNombre de TLM : ${acc.nbTLM}`)
                         .setColor("#006699");
                 }
             }
-            
+            embed.addField(`Total: `,`Nombre de WAX : ${nbWax}\nNombre de TLM : ${nbTlm}`)
             message.channel.send(embed);
         }else {
             client.updateBalance(args[0],message.guild);
