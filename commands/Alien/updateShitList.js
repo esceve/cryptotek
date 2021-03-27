87ee6aacab19868dabc3f5fe46d7222a9831cefa
akkoucai <aimene.akkouche@grenoble-inp.org>
2021-03-27 18:26:16 +0100

    updateshitlist activated

--- commands/Alien/updateShitList.js
+++ commands/Alien/updateShitList.js
@@ -2,7 +2,7 @@ const { MESSAGES } = require("../../util/constants");
 const {MessageEmbed} = require('discord.js');
 
 module.exports.run = async (client,message,args) => {
-        //await client.updateShitlist();
+        await client.updateShitlist();
 
 
 };
