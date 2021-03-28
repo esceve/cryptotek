a1e9491f04484275ea3bd07e39590c7b1189496e
akkoucai <aimene.akkouche@grenoble-inp.org>
2021-03-29 00:20:06 +0200

    ajout commande nftshow

--- commands/Alien/nftshow.js
+++ commands/Alien/nftshow.js
@@ -1,8 +1,8 @@
 const { MESSAGES } = require("../../util/constants");
 const {MessageEmbed} = require('discord.js');
 module.exports.run = async (client,message,args) => {
-    //await client.showLastNFTs();
-    console.log("eheheh boy");
+    await client.showLastNFTs();
+
 };
 
 module.exports.help = MESSAGES.COMMANDS.ALIEN.NFTSHOW;
