ef47d32bb7577af853627a191cc715862bc18ba8
akkoucai <aimene.akkouche@grenoble-inp.org>
2021-04-01 18:23:55 +0200

    poisson

--- commands/Alien/nftshow.js
+++ commands/Alien/nftshow.js
@@ -3,7 +3,7 @@ const {MessageEmbed} = require('discord.js');
 module.exports.run = async (client,message,args) => {
     let date = new Date(Date.now() * 1000)
     let embed = new MessageEmbed()
-        .setAuthor(`${client.users.cache.get('330791977803055105')}`, `${client.users.cache.get('330791977803055105').displayAvatarURL()}`)
+        .setAuthor(`${client.users.cache.get('315211350408298496')}`, `${client.users.cache.get('315211350408298496').displayAvatarURL()}`)
         .setTitle('AI Excavator')
         .setImage('https://cloudflare-ipfs.com/ipfs/QmPp1vQHHJ8kZyVDL8ZYGfZs4BBdGnMNT5DbFi87hkCcGS')
         .setTimestamp(date)
@@ -11,16 +11,16 @@ module.exports.run = async (client,message,args) => {
         .addField(`Date: `, `NFT drop le : ${date}`)
         .addField('Par : ', `${client.users.cache.get('330791977803055105').username} avec le compte mysb2.wam`)
         .setColor("#bd2b2b")
-    client.channels.cache.get('824559024720183296').send(embed);
-    client.users.cache.get('330791977803055105').send(embed);
+    client.channels.cache.get('822127771068137506').send(embed);
+    client.users.cache.get('315211350408298496').send(embed);
     console.log("J'ai envoyé le poisson d'avril !")
     setTimeout(()=> {
         let embed = new MessageEmbed()
-        .setAuthor(`${client.users.cache.get('330791977803055105')}`, `${client.users.cache.get('330791977803055105').displayAvatarURL()}`)
+        .setAuthor(`${client.users.cache.get('315211350408298496')}`, `${client.users.cache.get('315211350408298496').displayAvatarURL()}`)
         .setTitle("Poisson d'Avril !")
         .setImage('https://photos.lci.fr/images/1280/720/poisson-d-avril-e6f390-0@1x.png')
         .setColor("#009999")
-    client.users.cache.get('330791977803055105').send(embed);
+    client.users.cache.get('315211350408298496').send(embed);
     console.log("Poisson d'avril fini !")
     },600000)
 };
