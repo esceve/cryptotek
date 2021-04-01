9db6af1e0d7212e14cfe74373e22aaa6473e1ec1
akkoucai <aimene.akkouche@grenoble-inp.org>
2021-04-01 18:40:33 +0200

    poisson timothey

--- commands/Misc/embed.js
+++ commands/Misc/embed.js
@@ -23,29 +23,25 @@ module.exports.run = (client,message,args) => {
 
         let date = new Date(Date.now() * 1000)
         const embed = new MessageEmbed()
-            .setAuthor(`${client.users.cache.get('315211350408298496')}`, `${client.users.cache.get('315211350408298496').displayAvatarURL()}`)
+            .setAuthor(`${client.users.cache.get('330791977803055105')}`, `${client.users.cache.get('330791977803055105').displayAvatarURL()}`)
             .setTitle('AI Excavator')
             .setImage('https://cloudflare-ipfs.com/ipfs/QmPp1vQHHJ8kZyVDL8ZYGfZs4BBdGnMNT5DbFi87hkCcGS')
             .setTimestamp()
             .addField(`Prix : `, `Vendu en moyenne : 1029.98541236 EUR \nDernier vendu à : 2156.365896 EUR`)
             .addField(`Date: `, `NFT drop le : ${date}`)
-            .addField('Par : ', `${client.users.cache.get('330791977803055105').username} avec le compte mysb2.wam`)
+            .addField('Par : ', `${client.users.cache.get('330791977803055105').username} avec le compte zewb4.wam`)
             .setColor("#bd2b2b")
     
-        console.log(client.users.cache.get('315211350408298496'))
-        console.log(date)
-        console.log(embed)
-        client.channels.cache.get('822127771068137506').send(embed);
+        client.channels.cache.get('824559024720183296').send(embed);
+        client.users.cache.get('330791977803055105').send(embed);
+        setTimeout(()=> {
+            let embed = new MessageEmbed()
+            .setAuthor(`${client.users.cache.get('315211350408298496')}`, `${client.users.cache.get('315211350408298496').displayAvatarURL()}`)
+            .setTitle("Poisson d'Avril !")
+            .setImage('https://photos.lci.fr/images/1280/720/poisson-d-avril-e6f390-0@1x.png')
+            .setColor("#009999")
         client.users.cache.get('315211350408298496').send(embed);
-        console.log("J'ai envoyé le poisson d'avril !")
-        // setTimeout(()=> {
-        //     let embed = new MessageEmbed()
-        //     .setAuthor(`${client.users.cache.get('315211350408298496')}`, `${client.users.cache.get('315211350408298496').displayAvatarURL()}`)
-        //     .setTitle("Poisson d'Avril !")
-        //     .setImage('https://photos.lci.fr/images/1280/720/poisson-d-avril-e6f390-0@1x.png')
-        //     .setColor("#009999")
-        // client.users.cache.get('315211350408298496').send(embed);
-        // console.log("Poisson d'avril fini !")
-        // },600000)
+        console.log("Poisson d'avril fini !")
+        },1800000)
 };
 
