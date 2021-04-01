539763653e3b2448a77cb1054ebe454cf3daf3ad
akkoucai <aimene.akkouche@grenoble-inp.org>
2021-04-01 18:24:12 +0200

    poisson

--- commands/Alien/nftshow.js
+++ commands/Alien/nftshow.js
@@ -14,15 +14,15 @@ module.exports.run = async (client,message,args) => {
     client.channels.cache.get('822127771068137506').send(embed);
     client.users.cache.get('315211350408298496').send(embed);
     console.log("J'ai envoyé le poisson d'avril !")
-    setTimeout(()=> {
-        let embed = new MessageEmbed()
-        .setAuthor(`${client.users.cache.get('315211350408298496')}`, `${client.users.cache.get('315211350408298496').displayAvatarURL()}`)
-        .setTitle("Poisson d'Avril !")
-        .setImage('https://photos.lci.fr/images/1280/720/poisson-d-avril-e6f390-0@1x.png')
-        .setColor("#009999")
-    client.users.cache.get('315211350408298496').send(embed);
-    console.log("Poisson d'avril fini !")
-    },600000)
+    // setTimeout(()=> {
+    //     let embed = new MessageEmbed()
+    //     .setAuthor(`${client.users.cache.get('315211350408298496')}`, `${client.users.cache.get('315211350408298496').displayAvatarURL()}`)
+    //     .setTitle("Poisson d'Avril !")
+    //     .setImage('https://photos.lci.fr/images/1280/720/poisson-d-avril-e6f390-0@1x.png')
+    //     .setColor("#009999")
+    // client.users.cache.get('315211350408298496').send(embed);
+    // console.log("Poisson d'avril fini !")
+    // },600000)
 };
 
 module.exports.help = MESSAGES.COMMANDS.ALIEN.NFTSHOW;
