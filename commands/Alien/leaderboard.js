d86e7819ef22eeb242dd3cb839e1c35a0b80fbf3
akkoucai <aimene.akkouche@grenoble-inp.org>
2021-04-03 02:55:57 +0200

    fix lb

--- commands/Alien/leaderboard.js
+++ commands/Alien/leaderboard.js
@@ -1,64 +1,62 @@
 const { MESSAGES } = require("../../util/constants");
-//const { User} = require("../../models/index");
+const { User} = require("../../models/index");
 const {MessageEmbed} = require("discord.js");
-//const {User} = require("../../models/user");
+
 
 module.exports.run = async (client, message, args,settings,dbUser) => {
-    console.log('LEADERBOOOOOOOARD')
-    // let embed = new MessageEmbed()
-    //     .setAuthor(
-    //         `${message.member.displayName}`,
-    //         message.member.user.displayAvatarURL()
-    //     )
-    //     .setTimestamp();
+    let embed = new MessageEmbed()
+        .setAuthor(
+            `${message.member.displayName}`,
+            message.member.user.displayAvatarURL()
+        )
+        .setTimestamp();
 
-    // let users = await User.find({});
-    // let usersLeadboard = [];
-    // for (const user in users) {
-    //     let nbWax = 0;
-    //     let nbTlm = 0;
-    //     for(const accName of users[user].accounts){
-    //         let acc = await client.getAccount(accName);
-    //         console.log(`Lb account : ${acc}`)
-    //         nbWax += parseFloat(acc.nbWAX);
-    //         nbTlm += parseFloat(acc.nbTLM);
-    //     }
-    //     let nbWaxEUR = await client.waxPrice();
-    //     let nbTlmEUR = await client.tlmPrice();
-    //     var tlmToWax = nbTlm * nbTlmEUR;
-    //     var totalWax = tlmToWax + nbWax;
-    //     var WaxToEur = totalWax * nbWaxEUR;
-    //     if (users[user].guildName === message.guild.name) {
-    //         usersLeadboard.push({
-    //             username: users[user].username,
-    //             nbrAccount: users[user].accounts.length,
-    //             wax: nbWax,
-    //             tlm: nbTlm,
-    //             eur: WaxToEur
-    //         })
-    //     }
-    // }
-    // usersLeadboard.sort(function (a, b) {
-    //     if ( a.eur < b.eur ){
-    //         return 1;
-    //     }
-    //     if ( a.eur > b.eur ){
-    //         return -1;
-    //     }
-    //     return 0
-    // })
-    // for (const user in usersLeadboard) {
-    //     embed.
-    //         addField(
-    //             `${usersLeadboard[user].username} N°${i}`,
-    //             `WAX: ${usersLeadboard[user].wax.toFixed(2)} WAX
-    //                 TLM: ${usersLeadboard[user].tlm.toFixed(2)} TLM
-    //                 EUR: ${usersLeadboard[user].eur.toFixed(2)} EUR
-    //                 Accounts: ${usersLeadboard[user].nbrAccount}`
-    //         )      
-    // }
-    // console.log(usersLeadboard)
-    // message.channel.send(embed);
+    let users = await User.find({});
+    let usersLeadboard = [];
+    for (const user in users) {
+        let nbWax = 0;
+        let nbTlm = 0;
+        for(const accName of users[user].accounts){
+            let acc = await client.getAccount(accName);
+            console.log(`Lb account : ${acc}`)
+            nbWax += parseFloat(acc.nbWAX);
+            nbTlm += parseFloat(acc.nbTLM);
+        }
+        let nbWaxEUR = await client.waxPrice();
+        let nbTlmEUR = await client.tlmPrice();
+        var tlmToWax = nbTlm * nbTlmEUR;
+        var totalWax = tlmToWax + nbWax;
+        var WaxToEur = totalWax * nbWaxEUR;
+        if (users[user].guildName === message.guild.name) {
+            usersLeadboard.push({
+                username: users[user].username,
+                nbrAccount: users[user].accounts.length,
+                wax: nbWax,
+                tlm: nbTlm,
+                eur: WaxToEur
+            })
+        }
+    }
+    usersLeadboard.sort(function (a, b) {
+        if ( a.eur < b.eur ){
+            return 1;
+        }
+        if ( a.eur > b.eur ){
+            return -1;
+        }
+        return 0
+    })
+    for (const user in usersLeadboard) {
+        embed.
+            addField(
+                `${usersLeadboard[user].username} N°${i}`,
+                `WAX: ${usersLeadboard[user].wax.toFixed(2)} WAX
+                    TLM: ${usersLeadboard[user].tlm.toFixed(2)} TLM
+                    EUR: ${usersLeadboard[user].eur.toFixed(2)} EUR
+                    Accounts: ${usersLeadboard[user].nbrAccount}`
+            )      
+    }
+    message.channel.send(embed);
 };
 
 module.exports.help = MESSAGES.COMMANDS.ALIEN.LEADERBOARD;
--- commands/Misc/test.js
+++
@@ -1,7 +0,0 @@
-const {MESSAGES} = require('../../util/constants');
-
-module.exports.help = MESSAGES.COMMANDS.MISC.TEST;
-
-module.exports.run = (client,message,args) => {
-    message.channel.send(args.join(" "));
-};
