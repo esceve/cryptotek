72c17ed716fd4f0ee7550efe18f83ff79bbcee29
esceve <clement62880@gmail.com>
2021-04-06 19:31:14 +0200

    enable https

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const { loadCommands, loadEvents } = require("./util/loader");
 
 
 const http = require("http");
+const https = require("https");
 const express = require("express");
 const cors = require("cors");
 const apiInterfaces = require('./Interfaces/Front_interfaces');
@@ -19,14 +20,30 @@ bot.mongoose = require("./util/mongoose");
 
 
 //API IMPLEMENTATION
-const PORT = 8081;
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/stonk.retrogala.ovh/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/stonk.retrogala.ovh/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/stonk.retrogala.ovh/chain.pem', 'utf8');
+
+const creditentials = {
+   key: privateKey,
+   cert: certificate,
+   ca: ca
+};
+
+const PORT = 80;
+const PORTHTTPS = 443;
 const app = express();
 app.use(express.json());
-const server = http.Server(app);
+const httpServer= http.Server(app);
+const httpsServer = https.Server(creditentials, app);
 app.use(cors({ credentials: true, origin: true, exposedHeaders: ['x-auth-token'] }));
 app.use('/api', apiInterfaces);
 
-server.listen(PORT, function () {
+httpServer.listen(PORT, function () {
+  console.log(`Server running on port: ${PORT}`);
+});
+httpsServer.listen(PORT, function () {
   console.log(`Server running on port: ${PORT}`);
 });
 
