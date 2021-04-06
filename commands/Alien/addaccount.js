384fddf9a401309cb90b1d4be0953dd512c9128a
akkoucai <aimene.akkouche@grenoble-inp.org>
2021-04-06 17:47:01 +0200

    fix mkacc

--- commands/Alien/addaccount.js
+++ commands/Alien/addaccount.js
@@ -6,22 +6,10 @@ module.exports.run = async (client,message,args,settings,dbUser) => {
                 .setAuthor(`${message.member.displayName} (${message.member.id})`,message.member.user.displayAvatarURL())
                 .setColor("#92f058")
                 .setTimestamp();
-    let embed2 = new MessageEmbed()
-                .setAuthor(`${message.member.displayName} (${message.member.id})`,message.member.user.displayAvatarURL())
-                .setColor("#92f058")
-                .setTimestamp();
-    let i = 0;
     for(const accName of args){
-        i++;
         let data = await client.getAccount(accName);
-
-        if(data){
-            if(i > 24){
-                embed2.addField(`Bot `, `${accName} a déjà été ajouté par ${data.username}.`)
-            }else{
-                embed.addField(`Bot `, `${accName} a déjà été ajouté par ${data.username}.`)
-            }
-            
+        if(data){ // Cas où le compte a été déjà créé
+                embed.addField(`Bot `, `${accName} a déjà été ajouté par ${data.username}.`) 
         }else { // Cas où le compte n'a pas été créé
             const compteExiste = await client.accountExist(accName,message.guild);
             if (compteExiste) { //On l'ajoute si et seulement si il existe
@@ -32,25 +20,15 @@ module.exports.run = async (client,message,args,settings,dbUser) => {
                 let acc = dbUser.accounts;
                 if(!acc.includes(accName)) acc.push(accName);
                 await client.updateUser(message.member.user, {accounts: acc});
-                if(i>24){
-                    embed2.addField(`Bot `,`${accName} ajouté avec succès`)
-                }else{
-                    embed.addField(`Bot `,`${accName} ajouté avec succès`)
-                }
-                
+                embed.addField(`Bot `,`${accName} ajouté avec succès`)
                 
             }else {
-                if(i>24){
-                    embed2.addField(`Bot `, `${accName} n'existe pas.`)
-                }else{
-                    embed.addField(`Bot `, `${accName} n'existe pas.`)
-                }
+                embed.addField(`Bot `, `${accName} n'existe pas.`)
             }
 
         }
     } 
     message.channel.send(embed);
-    if(i>24) message.channel.send(embed2);
     
 };
 
