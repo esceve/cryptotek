b3f56bf8ae9e8d04a1ade731dded567ebc1ec4d3
akkoucai <aimene.akkouche@grenoble-inp.org>
2021-04-08 00:31:00 +0200

    fix bot

--- commands/Alien/nftshow.js
+++ commands/Alien/nftshow.js
@@ -1,7 +1,7 @@
 const { MESSAGES } = require("../../util/constants");
 const {MessageEmbed} = require('discord.js');
 module.exports.run = (client,message,args) => {
-   await client.showLastNFTs()
+   await client.showLastNFTs();
 };
 
 module.exports.help = MESSAGES.COMMANDS.ALIEN.NFTSHOW;
